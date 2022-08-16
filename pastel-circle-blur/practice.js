3e8a5bf01c51bdcfe86033eeafc9cf29a422b66f
dengoyoon <engus927@naver.com>
2022-08-16 23:33:34 +0900

    명령어 함수화 진행 리팩토링

--- pastel-circle-blur/practice.js
+++ pastel-circle-blur/practice.js
@@ -1,38 +1,42 @@
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
 
-const canvasWidth = 1200;
-const canvasHeight = 700;
+const canvasWidth = 1400;
+const canvasHeight = 750;
 
 const log = console.log;
 const pi = Math.PI;
 
-let radius;
-
-const lineNum = 300;
-
 const getRandomX = () => Math.floor(Math.random() * canvasWidth) + 0;
 const getRandomY = () => Math.floor(Math.random() * canvasHeight) + 0;
 const getColorNum = () => Math.floor(Math.random() * 70) + 185;
+const getRandomRGB = () =>
+  `rgb(${getColorNum()}, ${getColorNum()}, ${getColorNum()}, ${getRandomOpacity()})`;
 const getRandomRadius = () => Math.floor(Math.random() * 200) + 100;
 const getRandomOpacity = () => Math.random();
 
-const drawLineCircle = (centerX, centerY) => {
-  ctx.strokeStyle = `rgb(${getColorNum()}, ${getColorNum()}, ${getColorNum()}, ${getRandomOpacity()})`;
+const drawLine = (sX, sY, eX, eY) => {
+  ctx.beginPath();
+  ctx.moveTo(sX, sY);
+  ctx.lineTo(eX, eY);
+  ctx.stroke();
+  ctx.closePath();
+};
+
+const drawLineCircle = (centerX, centerY, radius, lineNum) => {
+  ctx.strokeStyle = getRandomRGB();
   radius = getRandomRadius();
   for (let i = 0; i <= lineNum; i += 1) {
-    ctx.beginPath();
     const radian = (2 * pi * i) / lineNum;
-    ctx.moveTo(centerX, centerY);
-    ctx.lineTo(
+    drawLine(
+      centerX,
+      centerY,
       centerX + radius * Math.cos(radian),
       centerY + radius * Math.sin(radian)
     );
-    ctx.stroke();
-    ctx.closePath();
   }
 };
 
 for (let i = 0; i <= 200; i++) {
-  drawLineCircle(getRandomX(), getRandomY());
+  drawLineCircle(getRandomX(), getRandomY(), getRandomRadius(), 300);
 }
